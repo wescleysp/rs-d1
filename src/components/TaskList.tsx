8db8bfbb7534c703b760cbd2d6fc6d64e5309cf9
Wescley Serrão <wescleysp@gmail.com>
2021-03-29 07:49:33 -0300

    :tada: project finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import { v4 as uuidv4 } from 'uuid';
+
 import '../styles/tasklist.scss'
 
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
@@ -16,14 +18,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const id = uuidv4();
+    const newTask: Task = {
+      id: id,
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTask = tasks.filter(task => task.id !== id);
+    setTasks(newTask);
+
   }
 
   return (
